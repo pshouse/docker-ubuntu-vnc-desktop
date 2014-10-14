19998a4aa3f6296ca20f4d54a7084feec4d33544
Patrick Shouse <shouse.patrick@gmail.com>
2014-10-14 08:08:01 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir /var/run/sshd
+mkdir /run/sshd
 
 # create an ubuntu user
 PASS=`pwgen -c -n -1 10`
@@ -9,9 +9,14 @@ echo "User: ubuntu Pass: $PASS"
 useradd --create-home --shell /bin/bash --user-group --groups adm,sudo ubuntu
 echo "ubuntu:$PASS" | chpasswd
 
+# Copy Squeak files
 cp -a /Squeak-4.5-All-In-One/Squeak-4.5-All-in-One.app /home/ubuntu
+cp /Squeak-4.5-All-In-One/squeak.sh /home/ubuntu
+
+# Set Owner
 chown -R ubuntu:ubuntu /home/ubuntu/Squeak-4.5-All-in-One.app
 
+# Start process supervisor
 /usr/bin/supervisord -c /supervisord.conf
 
 while [ 1 ]; do
